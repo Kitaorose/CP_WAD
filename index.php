0acbbdb0df325da80dad166926f8ac4cb77ee56a
Kitaorose <Bakhita.Okong'o@strathmore.edu>
2024-07-07 16:36:51 +0300

    Edit navigation bar

--- index.php
+++ index.php
@@ -4,29 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
-    <div class="topnav a">
-        <a href="index.html">Home</a>
-        <a href="about.html">About Us</a>
-        <a href="activities.html">Activities</a> 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Activities</a></li>
-                <li><a class="dropdown-item" href="#">Activity rates</a></li>
-                </ul>
-            </li>
-        <a href="rooms.html">Rooms</a>
-        <a href="Contact.html">Contact us</a>
-        <div class="topnav-right">
-            <a href="si.html">Sign in</a>
-            <a href="su.html">Sign up</a>
-        </div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="Images/BBTLR Logo 0nly.jpg" alt="Bamboo by the lake resort logo" width="30" height="24">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="about.php">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="activities.php">Activities</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="rooms.php">Rooms</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Contact.php">Contact us</a>
+        </li>
+       </ul>
+        <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link" href="si.html">Sign in</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="su.html">Sign up</a>
+        </li>
+        </ul>
     </div>
+  </div>
+</nav>
 
     <div class="banner">
         <h1>Bamboo By The Lake Resort</h1>
@@ -79,5 +97,6 @@
         &copy;2024|Bamboo by The Lake.|All rights reserved.
    </div>
 
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
