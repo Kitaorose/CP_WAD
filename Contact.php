205f2a792295b24dd445dcd7fbd7925c3f95f963
Kitaorose <Bakhita.Okong'o@strathmore.edu>
2024-07-07 21:51:11 +0300

    Submit entered data

--- Contact.php
+++ Contact.php
@@ -11,21 +11,40 @@
     <?php
         require_once ("includes/db_connect.php");
         include_once ("Templates/nav.php");
+
+        if(isset($_POST["send_message"])){
+
+            $fn= mysqli_real_escape_string($conn,$_POST["fullname"]);
+            $Pn= mysqli_real_escape_string($conn,$_POST["phone number"]);
+            $Em= mysqli_real_escape_string($conn,$_POST["email"]);
+            $s= mysqli_real_escape_string($conn,$_POST["Subject_line"]);
+            $mes= mysqli_real_escape_string($conn,$_POST["send_message"]);
+        
+            $insert_message = "INSERT INTO messages (sender_name,sender_phone_number, sender_email,subject_line, text_message)
+            VALUES ('$fn', '$Pn', '$Em', '$s','$mes')";
+        
+            if ($conn->query($insert_message) === TRUE) {
+                header("Location: view_messages.php");
+                exit();
+            } else {
+                echo "Error: " . $insert_message . "<br>" . $conn->error;
+            }
+        }
         ?>
     <div class="banner"> <h1>Bamboo by the lake Resort</h1></div>
    <div class="row::after">
     <h1>Contact Us</h1>
     <p>Have any queries or ideas concerning our facilities?<br> Please leave us a message and we will be glad to respond ASAP.</p>
     <h3>Details</h3>
-<form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]);?>"method="post" class="contacts_form label">
-    <label for="f&Ln">Fullnames</label><br>
-    <input type="text" name="fullname" id="F&Ln " placeholder="First & Last Name"><br>
+<form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]);?>"method="post" class="contacts_form label">
+    <label for="fn">Fullnames</label><br>
+    <input type="text" name="fullname" id="fn " placeholder="First & Last Name" required><br><br>
 
     <label for="Pn">Phone Number</label><br>
-    <input type="number" number_format name="phone number" id="Pn" placeholder="Phone Number"><br>
+    <input type="tel" name="phone number" id="Pn"><br><br>
 
     <label for="Em">Email Address</label><br>
-    <input type="text" name="email" id="Em" placeholder="Email"><br><br>
+    <input type="text" name="email" id="Em" placeholder="Email" required><br><br>
 
     <label for="s">Subject</label><br>
                 <select name="Subject_line" id="s">
@@ -33,13 +52,17 @@
                     <option value="booking inquiry">Booking inquiry</option>
                     <option value="review">Review</option>
                 </select><br><br>
+
     <label for="mes">Message</label><br>
-    <textarea name="message" rows="4" cols= "50" id="M">Enter Message</textarea><br><br>
-    <input type="button" value="Send Message" ><br>
+    <textarea name="message" rows="4" cols= "50" id="mes" required>Enter Message</textarea><br><br>
+
+    <input type="submit" value="Send Message" name="send_message"><br><br>
+
     </form>
    </div>
    <div class="footer">
     &copy;2024|Bamboo by The Lake.|All rights reserved.
    </div>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
