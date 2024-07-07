15dabde1a8ea288734d82398f698dca1f62ac297
Kitaorose <Bakhita.Okong'o@strathmore.edu>
2024-07-07 16:42:54 +0300

    add template link for navigation bar

--- index.php
+++ index.php
@@ -8,44 +8,9 @@
     <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="#">
-      <img src="Images/BBTLR Logo 0nly.jpg" alt="Bamboo by the lake resort logo" width="30" height="24">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="./">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="about.php">About us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="activities.php">Activities</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="rooms.php">Rooms</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Contact.php">Contact us</a>
-        </li>
-       </ul>
-        <ul class="nav justify-content-end">
-        <li class="nav-item">
-            <a class="nav-link" href="si.html">Sign in</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="su.html">Sign up</a>
-        </li>
-        </ul>
-    </div>
-  </div>
-</nav>
-
+        <?php
+        include_once ("Templates/nav.php");
+        ?>
     <div class="banner">
         <h1>Bamboo By The Lake Resort</h1>
     </div>
