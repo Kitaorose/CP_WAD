2313e90917258bc7293c35f6e75a0c00f6e6a45b
Kitaorose <Bakhita.Okong'o@strathmore.edu>
2024-07-07 21:50:36 +0300

    end of bootsrap

--- about.php
+++ about.php
@@ -57,5 +57,6 @@
     <div class="footer" style="margin-block-start: 1400px;">
         &copy;2024|Bamboo by The Lake.|All rights reserved.
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
