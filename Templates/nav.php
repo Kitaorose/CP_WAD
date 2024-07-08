a5ebd7e50d1d5f050912d9f6a426d0a31b02f0ed
Kitaorose <Bakhita.Okong'o@strathmore.edu>
2024-07-08 12:39:48 +0300

    adjust logo

--- Templates/nav.php
+++ Templates/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
   <a class="navbar-brand" href="#">
-      <img src="Images/BBTLR Logo 0nly.jpg" alt="Bamboo by the lake resort logo" width="30" height="24">
+      <img src="Images/BBTLR Logo 0nly.jpg" alt="Bamboo by the lake resort logo" width="45" height="38">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
