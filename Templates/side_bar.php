1f3e9f6edc443f823393c9eb881e659711be1c12
Kitaorose <Bakhita.Okong'o@strathmore.edu>
2024-07-08 13:26:16 +0300

    Create a navbar template.

---
+++ Templates/side_bar.php
@@ -0,0 +1,17 @@
+<div class="side_bar">
+        <h3>Our Vision</h3>
+        <p>Nestled in the heart of Kenya’s breathtaking natural beauty, 
+            Bamboo by the Lake Resort offers an escape to paradise for those 
+            seeking a tranquil retreat away from the hustle and bustle of 
+            city life. Our resort is a haven of peace, located on the 
+            shores of Lake Victoria surrounded by lush greenery and 
+            teeming with wildlife.</p>
+            <br>
+        <h3>Our Mission</h3>
+        <p>At Bamboo by the Lake Resort, we believe in providing an authentic
+            Kenyan experience combined with the comforts of modern amenities.
+            Our commitment to sustainability and eco-friendly practices ensures 
+            that we live in harmony with the environment.</p>
+            <br>
+
+    </div>
