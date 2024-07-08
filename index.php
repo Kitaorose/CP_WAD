067008a3e33440d45ba348881ed1158fb11b700e
Kitaorose <Bakhita.Okong'o@strathmore.edu>
2024-07-08 14:07:51 +0300

    Deleted side bar and replaced with an include reused code

--- index.php
+++ index.php
@@ -4,23 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <!--page style links--> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/style.css">
+    <!--End of style links--> 
 </head>
 <body>
         <?php
-        include_once ("Templates/nav.php");
+        include_once ("Templates/nav.php");// navigation bar code.
         ?>
-    <div class="banner">
+    <div class="banner"><!--page header--> 
         <h1>Bamboo By The Lake Resort</h1>
     </div>
     <div class="row::after">
+        <!--Resort view image---> 
         <img src="Images/Resort rooms 2.png" alt="">
     </div>
     <div class="content">
         <div>
             <h3 style="text-align: center;">Check Availability</h3>
-    
+            <!-- Booking form for the user-->
             <form action="" style="text-align: center; background-image: image(Images/Resort rooms 2.png);"
             >
                 <label for="CI">Check In</label><br>
@@ -40,24 +43,12 @@
             </form>
         </div>
        
-    </div>
-    <div class="side_bar">
-        <h3>Our Vision</h3>
-        <p>Nestled in the heart of Kenya’s breathtaking natural beauty, 
-            Bamboo by the Lake Resort offers an escape to paradise for those 
-            seeking a tranquil retreat away from the hustle and bustle of 
-            city life. Our resort is a haven of peace, located on the 
-            shores of Lake Victoria surrounded by lush greenery and 
-            teeming with wildlife.</p>
-            <br>
-        <h3>Our Mission</h3>
-        <p>At Bamboo by the Lake Resort, we believe in providing an authentic
-            Kenyan experience combined with the comforts of modern amenities.
-            Our commitment to sustainability and eco-friendly practices ensures 
-            that we live in harmony with the environment.</p>
-            <br>
+    </div> <!--end of booking form-->
 
-    </div>
+    <?php
+    include_once ("Templates/side_bar.php");// side_bar repeated code.
+    ?>
+   
    <div class="footer" style="margin-block-start: 1000px;">
         &copy;2024|Bamboo by The Lake.|All rights reserved.
    </div>
